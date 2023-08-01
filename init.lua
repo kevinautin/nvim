9931808b20787ad0c1b5f2938145ae57ae90a3e5
kevinautin <kevin.autin@adriel.ai>
2023-08-01 17:56:47 +0900

    fixed nvim-dap-ui

--- init.lua
+++ init.lua
@@ -1,3 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.opt.clipboard = "unnamedplus"
+
+local opt = vim.opt
+opt.clipboard = "unnamedplus"
+opt.scrolloff = 200 -- Lines of context
+opt.wrap = false -- Disable line wrap
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -192,4 +192,68 @@ return {
       },
     },
   },
+  {
+    "rcarriga/nvim-dap-ui",
+  -- stylua: ignore
+  keys = {
+    { "<leader>du", function() require("dapui").toggle({ }) end, desc = "Dap UI" },
+    { "<leader>de", function() require("dapui").eval() end, desc = "Eval", mode = {"n", "v"} },
+  },
+    -- opts = {},
+    opts = {
+      layouts = {
+        {
+          elements = {
+            {
+              id = "scopes",
+              size = 0.20,
+            },
+            {
+              id = "watches",
+              size = 0.5,
+            },
+            {
+              id = "breakpoints",
+              size = 0.15,
+            },
+            {
+              id = "stacks",
+              size = 0.15,
+            },
+          },
+          position = "left",
+          size = 40,
+        },
+        {
+          elements = {
+            -- {
+            --   id = "repl",
+            --   size = 0.5,
+            -- },
+            {
+              id = "console",
+              -- size = 0.5,
+              size = 1,
+            },
+          },
+          position = "bottom",
+          size = 20,
+        },
+      },
+    },
+    config = function(_, opts)
+      local dap = require("dap")
+      local dapui = require("dapui")
+      dapui.setup(opts)
+      dap.listeners.after.event_initialized["dapui_config"] = function()
+        dapui.open({})
+      end
+      dap.listeners.before.event_terminated["dapui_config"] = function()
+        dapui.close({})
+      end
+      dap.listeners.before.event_exited["dapui_config"] = function()
+        dapui.close({})
+      end
+    end,
+  },
 }
