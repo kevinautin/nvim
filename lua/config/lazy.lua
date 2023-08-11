6e0bdea31eed1aa92e4189bf5077b67eaee532bc
Kevin Autin Martineau <kevin.autin@adriel.com>
2023-08-11 18:41:24 +0900

    yanky

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -17,6 +17,7 @@ require("lazy").setup({
     -- import/override with your plugins
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
+    { import = "lazyvim.plugins.extras.coding.yanky" },
     { import = "plugins" },
   },
   defaults = {
