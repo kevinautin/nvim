b63f665e1ebe5b15aa029682f04fe392f668132c
kevinautin <kevin.autin@adriel.ai>
2023-08-17 11:31:38 +0900

    eslint and prettier

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -18,6 +18,8 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.dap.core" },
     { import = "lazyvim.plugins.extras.coding.copilot" },
     { import = "lazyvim.plugins.extras.coding.yanky" },
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+    { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "plugins" },
   },
   defaults = {
