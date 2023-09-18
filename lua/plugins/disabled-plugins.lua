d0ee02cf30fe964c07cfb0e1c3f5dd33274f5e9d
Kevin Autin Martineau <kevin.autin@adriel.com>
2023-09-18 19:52:51 +0900

    disabled plugin file

---
+++ lua/plugins/disabled-plugins.lua
@@ -0,0 +1,5 @@
+return {
+  -- { "RRethy/vim-illuminate", enabled = false },
+  -- { "echasnovski/mini.ai", enabled = false },
+  -- { "nvim-treesitter/nvim-treesitter-textobjects" },
+}
--- lua/plugins/nvim-dap.lua
+++ lua/plugins/nvim-dap.lua
@@ -49,24 +49,9 @@ return {
             console = "integratedTerminal",
             restart = true,
             internalConsoleOptions = "neverOpen",
-            -- runtimeArgs = { "-r", "dotenv/config", "-r", "${workspaceFolder}/src/loadModuleAliases.js" },
             cwd = "${workspaceFolder}",
             resolveSourceMapLocations = { "${workspaceFolder}/**", "!**/node_modules/**" },
           },
-          -- {
-          --   type = "pwa-node",
-          --   request = "launch",
-          --   name = "Launch file",
-          --   program = "${file}",
-          --   cwd = "${workspaceFolder}",
-          -- },
-          -- {
-          --   type = "pwa-node",
-          --   request = "attach",
-          --   name = "Attach",
-          --   processId = require("dap.utils").pick_process,
-          --   cwd = "${workspaceFolder}",
-          -- },
         }
       end
     end,
