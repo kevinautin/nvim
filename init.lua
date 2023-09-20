954d03128ace5c13c9fa0fb48bd226910118280a
Kevin Autin Martineau <kevin.autin@adriel.com>
2023-09-20 10:24:19 +0900

    move to next function

--- init.lua
+++ init.lua
@@ -5,3 +5,15 @@ local opt = vim.opt
 opt.clipboard = "unnamedplus"
 opt.scrolloff = 200 -- Lines of context
 opt.wrap = false -- Disable line wrap
+
+-- mini.ai moves
+local map_ai_move = function(lhs, textobject_id, direction, desc)
+  local rhs = function()
+    MiniAi.move_cursor("left", "a", textobject_id, { search_method = direction })
+  end
+  vim.keymap.set({ "n", "x", "o" }, lhs, rhs, { desc = desc })
+end
+-- Instead of `'f'` use id of textobject you'd like to move.
+-- For more info see `:h MiniAi.move_cursor()`.
+map_ai_move("[m", "f", "prev", "Jump to prev function")
+map_ai_move("]m", "f", "next", "Jump to next function")
--- lua/plugins/disabled-plugins.lua
+++ lua/plugins/disabled-plugins.lua
@@ -1,5 +1,3 @@
 return {
   -- { "RRethy/vim-illuminate", enabled = false },
-  -- { "echasnovski/mini.ai", enabled = false },
-  -- { "nvim-treesitter/nvim-treesitter-textobjects" },
 }
