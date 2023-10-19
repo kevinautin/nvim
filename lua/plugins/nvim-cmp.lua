d8c87b96fc3710f34888f68952ae2f22235152ef
Kevin Autin Martineau <kevin.autin@adriel.com>
2023-10-19 12:51:03 +0900

    fixed cmp

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -21,8 +21,13 @@ return {
 
       local luasnip = require("luasnip")
       local cmp = require("cmp")
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "emoji" } }))
+      opts.completion = {
+        completeopt = "menu,menuone,noinsert,noselect,preview",
+      }
 
       opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<CR>"] = cmp.mapping.confirm({ select = false }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
             -- You could replace select_next_item() with confirm({ select = true }) to get VS Code autocompletion behavior
