b5589de3704b25176a63fc18c2493d52b93cd755
Kevin Autin Martineau <kevin.autin@adriel.com>
2023-10-27 17:22:09 +0900

    disabled hardtime

--- lua/plugins/hardtime.lua
+++ lua/plugins/hardtime.lua
@@ -1,11 +1,18 @@
 return {
   {
     "m4xshen/hardtime.nvim",
+    enabled = false,
     dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
     opts = {
       disabled_keys = {
         ["<Up>"] = {},
         ["<Down>"] = {},
+        ["<Right>"] = {},
+        ["<Left>"] = {},
+        ["h"] = {},
+        ["j"] = {},
+        ["k"] = {},
+        ["l"] = {},
       },
       disable_mouse = false,
       restriction_mode = "hint",
