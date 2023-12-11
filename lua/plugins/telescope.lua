7ac42f1ee1a4c213f0df500153e1e5b983a4f9d2
kevinautin <kautinmarti@gmail.com>
2023-12-11 14:35:17 +0900

    telescope buffer from first index

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    -- ...
+    keys = {
+      {
+        "<leader>,",
+        "<cmd>Telescope buffers sort_mru=true<cr>",
+        desc = "Switch Buffer",
+      },
+    },
+    -- ...
+  },
+}
